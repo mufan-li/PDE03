ee6f9328d8040d67fe948da1efda663f2c0653e0
mufan-li <mufan.li@mail.utoronto.ca>
2015-05-19 17:25:07 -0400

    fixed Ux and Uxy discretization terms

--- rhscfd2.m
+++ rhscfd2.m
@@ -37,8 +37,8 @@ uny = DirechletBC(gridx(nx+1),gridy)';
 
 % corner units in the y direction size (ny-1)
 % used to remove the extra corners in cross derivatives
-uc0y = [u0y(1); zeros(ny-3,1); u0y(ny+1)];
-ucny = [uny(1); zeros(ny-3,1); uny(ny+1)];
+uc0y = [u0y(1); zeros(ny-3,1); -u0y(ny+1)];
+ucny = [-uny(1); zeros(ny-3,1); uny(ny+1)];
 
 % need to add the other coefficients
 % note - ux0 and uxn are terms used for uyy
@@ -48,9 +48,9 @@ rhs_add2y = - (kron(ux0(2:nx),vfy) + kron(uxn(2:nx),vly)) ./ hy^2;
 rhs_addxy = - (kron(vfx,u0y(1:ny-1)-u0y(3:ny+1)-uc0y) + ...
     kron(vlx,-uny(1:ny-1)+uny(3:ny+1)-ucny) + ...
     kron(ux0(1:nx-1)-ux0(3:nx+1),vfy) + ...
-    kron(-uxn(1:nx-1)-ux0(3:nx+1),vly)) ./ (4*hx*hy);
+    kron(-uxn(1:nx-1)+uxn(3:nx+1),vly)) ./ (4*hx*hy);
 
-rhs_add1x = - (kron(vfx,-u0y(2:ny)) + kron(uny(2:ny),vlx)) ./ (2*hx);
+rhs_add1x = - (kron(vfx,-u0y(2:ny)) + kron(vlx,uny(2:ny))) ./ (2*hx);
 rhs_add1y = - (kron(-ux0(2:nx),vfy) + kron(uxn(2:nx),vly)) ./ (2*hy);
 
 rhs = rhs + coefs(:,2) .* rhs_add1x + coefs(:,3) .* rhs_add2x ...
--- script2.m
+++ script2.m
@@ -10,20 +10,18 @@ ntimes = 5;
 nodex = 2.^(2:ntimes+1);
 nodey = nodex;
 
-UnoList = 0:2;
+% UnoList = 0:2;
 PDEnoList = 0:4;
-% UnoList=0;
-% PDEnoList=0;
+UnoList=0;
+% PDEnoList=4;
 
-for Uno = UnoList
-	for PDEno = PDEnoList
+for PDEno = PDEnoList
+	for Uno = UnoList
 		script;
 
 		disp(strcat([PDEname,', u = ',Uname]));
 		disp(errg);
-		errgd = errg(2:ntimes)-errg(1:ntimes-1);
-		disp(errgd);
-		errgr = errgd(1:ntimes-2) ./ errgd(2:ntimes-1);
+		errgr = errg(1:ntimes-1) ./ errg(2:ntimes);
 		disp(errgr);
 	end
 end
